07c53870cc31cce66fcbcab3b8a12835ea3fb6c4
Brad Thorson <bbthorson@gmail.com>
2020-02-24 11:04:15 -0500

    Fifth commit, redeleted src

--- src/utils/formatPrice.js
+++
@@ -1,9 +0,0 @@
-import config from '../../gatsbystorefront-config';
-const { locales, currency } = config;
-
-export default function formatPrice(price) {
-  return new Intl.NumberFormat(locales, {
-    style: 'currency',
-    currency,
-  }).format(price);
-}
